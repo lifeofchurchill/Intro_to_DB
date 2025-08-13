3ef151b765130c2fa5a292e86ce30dbbabe447d3
Churchill <reghoyah@gmail.com>
2025-08-13 04:10:25 +0000

    restructured to accomodate the foreign key refs

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,35 +2,35 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
-book_id INT PRIMARY KEY, 
+book_id INT AUTO_INCREMENT PRIMARY KEY, 
 title VARCHAR(130) NOT NULL,
-author_id INT,  
+FOREIGN KEY (author_id) REFERENCES Customer(customer_id),  
 price DOUBLE,
 publication_date DATE 
 );
 
 
 CREATE TABLE Authors (
-author_id INT PRIMARY KEY,
+author_id INT AUTO_INCREMENT PRIMARY KEY,
 author_name VARCHAR(215) NOT NULL 
 );
 
 CREATE TABLE Customers (
-customer_id INT PRIMARY KEY,
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
 customer_name VARCHAR(215) NOT NULL,
 email VARCHAR(215) UNIQUE,
 address TEXT NOT NULL
 );
 
 CREATE TABLE Orders (
-order_id INT PRIMARY KEY,
-customer_id INT,
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+FOREIGN KEY (customer_id) REFERENCES Customer (customer_id),
 order_date DATE
 );
 
 CREATE TABLE order_details (
-orderdetailid INT PRIMARY KEY,
-order_id INT,
-book_id INT,
+orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+FOREIGN KEY (book_id) REFERENCES Book (book_id),
 quantity DOUBLE
 );
