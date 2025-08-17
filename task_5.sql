379966fba1e1ceb4b096553c59a8d82699a5f2a2
Churchill <reghoyah@gmail.com>
2025-08-17 09:44:27 +0000

    updated table name to fit alx criteria

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_name, email, address)
+INSERT INTO Customer (customer_name, email, address)
 VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
 SELECT * FROM Customers;
